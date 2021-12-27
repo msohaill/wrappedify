53af911c2e0626dcae627a5effe5e16723b934ca
muhammadsohail <mhsohail56@gmail.com>
2021-12-27 18:35:11 -0500

    Fixed some things

--- wrappedify/settings.py
+++ wrappedify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [BASE_DIR / 'assets']
 
